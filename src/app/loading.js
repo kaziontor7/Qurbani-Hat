8d76b6c2903cb0f27eaa5b545ce2cf6b456f7568
kaziontor7 <kaziontor7@gmail.com>
2026-05-04 23:49:11 +0600

    fixed

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,10 +3,10 @@ import { Spinner } from "@heroui/react";
 
 const loading = () => {
     return (
-        <div className="flex items-center justify-center">
-             <div className="flex flex-col items-center gap-2">
+        <div className="flex items-center justify-center h-screen">
+             <div className="flex flex-col items-center gap-2 justify-center h-screen">
         <Spinner size="lg" />
-        <span className="text-xs text-muted">Large</span>
+        <span className="text-xs text-muted"></span>
       </div>
         </div>
     );
